6ffc51ccf547220803ea784f7644e0c568cc2bd9
munir79 <jakirhossainmunir79@gmail.com>
2023-08-03 13:42:52 +0600

    router setup in authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Registar from './Registar/Registar';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './Layout/Main';
+import Login from './Registar/Login';
 
 function App() {
  
-
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/registar',
+        element:<Registar></Registar>
+      }
+    ]
+  }
+])
   return (
     <div className="App">
   
-    <Registar></Registar>
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
