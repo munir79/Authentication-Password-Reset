0844d4bda4e29de092a65cfe6d690ca04ae03178
munir79 <jakirhossainmunir79@gmail.com>
2023-08-03 13:12:27 +0600

    password condition

--- src/Registar/Registar.js
+++ src/Registar/Registar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import app from '../Firebase/Firebase.init';
@@ -8,13 +8,34 @@ const auth=getAuth(app);
 
 
 const Registar = () => {
-
+         
+    const [passwordError,setPasswordError]=useState('');
 
     const handleRegistar=(event)=>{
         event.preventDefault();
         const email=event.target.email.value;
              const password=event.target.password.value;
              console.log(email,password);
+
+             if ( !/(?=.*[A-Z])/.test(password) ){
+                setPasswordError('Provide at least one Uppercse Character');
+                return;
+             }
+             if(password.length<6){
+                setPasswordError('please provide at lest 6 character');
+                return;
+             }
+
+             if(!/(?=.*[!#$%&? "])/.test(password)){
+                setPasswordError('please provide at least one special character');
+                return;
+             }
+
+             setPasswordError('');
+
+
+
+
              createUserWithEmailAndPassword(auth ,email,password)
              .then(result=>{
                 const user=result.user;
@@ -27,8 +48,9 @@ const Registar = () => {
     return (
         <div>
             <form onSubmit={handleRegistar}>
-                <input type="email"   name="email" id=""  placeholder='enter your email'/> <br />
-                <input type="password"  name="password" id="" placeholder='enter your password' /><br />
+                <input type="email"   name="email" id=""  required placeholder='enter your email'/> <br />
+                <input type="password"  name="password"   required id="" placeholder='enter your password' /><br />
+                <p>{passwordError} </p>
                 <button type="submit">Registar</button>
             </form>
         </div>
