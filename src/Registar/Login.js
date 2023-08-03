420b1c06bb858c9ae3f593d0046703976422213e
munir79 <jakirhossainmunir79@gmail.com>
2023-08-03 14:40:49 +0600

    reset password

--- src/Registar/Login.js
+++ src/Registar/Login.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {getAuth, sendPasswordResetEmail, signInWithEmailAndPassword} from 'firebase/auth';
 import app from '../Firebase/Firebase.init';
 const auth=getAuth(app);
 
 const Login = () => {
 
     const [success,setSuccess]=useState(false);
+    const [userEmail,setUserEmail]=useState('');
     const handleLogin=event=>{
         setSuccess(false);
         event.preventDefault();
@@ -26,17 +27,32 @@ const Login = () => {
         })
         
     }
+    const handleResetEmailBlur=event=>{
+        const email=event.target.value;
+        setUserEmail(email);
+    }
+
+    const handleResetPassword=()=>{
+        sendPasswordResetEmail(auth,userEmail)
+        .then(()=>{
+            alert('cheek your email and reset your password');
+        })
+        .catch(error=>{
+            console.log('error:',error);
+        })
+    }
     return (
         <div>
             <h1> Log in Here </h1>
             
 
             <form onSubmit={handleLogin}>
-                <input type="email" name="email" id="" placeholder='enter your email' /><br />
+                <input  onBlur={handleResetEmailBlur} type="email" name="email" id="" placeholder='enter your email' /><br />
                 <input type="password" name="password" id="" placeholder='enter your password' /><br />
                  {success && <p>sucessfully Log in </p>}
                 <button type="submit">Login  </button> <br />
-              <p>new this website<Link to='/registar' > Registar Now </Link> </p>
+              <p>new this website<Link to='/registar' > Registar Now </Link> </p> <br />
+              <p> forget Password ? <Link onClick={handleResetPassword} > Reset Password</Link></p>
             </form>
         </div>
     );
